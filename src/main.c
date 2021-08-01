05ff9a5b5779643c0d194ef8b144a6739e914b77
theteachr <sop.theteachr@gmail.com>
2021-08-01 20:07:09 +0530

    move to `src`

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <trie.h>
+
+int main(void) {
+	char *zip_code_data[] = {
+		"781109",
+		"560782",
+		"789663"
+	};
+
+	struct Trie zip_codes;
+
+	init_trie(&zip_codes);
+
+	for (int i = 0; i < 3; i++)
+		insert(&zip_codes, zip_code_data[i]);
+
+	print_trie(&zip_codes);
+
+	char* key = "560782";
+
+	printf("%s was%sfound.\n", key, search(&zip_codes, key) ? " " : " not ");
+
+	return 0;
+}
